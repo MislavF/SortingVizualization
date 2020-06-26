4af751aa416dcfdef67ca262ba33116d41f0ccd9
MislavF <mislavf.mf@gmail.com>
2020-06-26 23:02:44 +0200

    Pocetak projekta vizualizacije algoritama za sortiranje.

---
+++ SortingVizualization/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SortingVizualization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlSort = new System.Windows.Forms.PictureBox();
+            this.cmdRandomize = new System.Windows.Forms.Button();
+            this.cmdSort = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlSort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSort
+            // 
+            this.pnlSort.BackColor = System.Drawing.Color.White;
+            this.pnlSort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSort.Location = new System.Drawing.Point(12, 12);
+            this.pnlSort.Name = "pnlSort";
+            this.pnlSort.Size = new System.Drawing.Size(495, 310);
+            this.pnlSort.TabIndex = 0;
+            this.pnlSort.TabStop = false;
+            // 
+            // cmdRandomize
+            // 
+            this.cmdRandomize.Location = new System.Drawing.Point(582, 89);
+            this.cmdRandomize.Name = "cmdRandomize";
+            this.cmdRandomize.Size = new System.Drawing.Size(90, 42);
+            this.cmdRandomize.TabIndex = 1;
+            this.cmdRandomize.Text = "Randomize";
+            this.cmdRandomize.UseVisualStyleBackColor = true;
+            this.cmdRandomize.Click += new System.EventHandler(this.cmdRandomize_Click);
+            // 
+            // cmdSort
+            // 
+            this.cmdSort.Location = new System.Drawing.Point(582, 182);
+            this.cmdSort.Name = "cmdSort";
+            this.cmdSort.Size = new System.Drawing.Size(90, 42);
+            this.cmdSort.TabIndex = 2;
+            this.cmdSort.Text = "Sort";
+            this.cmdSort.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 334);
+            this.Controls.Add(this.cmdSort);
+            this.Controls.Add(this.cmdRandomize);
+            this.Controls.Add(this.pnlSort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pnlSort)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pnlSort;
+        private System.Windows.Forms.Button cmdRandomize;
+        private System.Windows.Forms.Button cmdSort;
+    }
+}
+
---
+++ SortingVizualization/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingVizualization
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        ArrayList array;
+
+        static Random randomNumber = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawSamples()
+        {
+            graphics.Clear(Color.White);
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                int x = (int)(((double)pnlSort.Width/array.Count)*i);
+
+                Pen pen = new Pen(Color.Black);
+                graphics.DrawLine(pen, new Point(x, pnlSort.Height), new Point(x, (int)(pnlSort.Height - (int)array[i])));
+            }
+        }
+
+        private void cmdRandomize_Click(object sender, EventArgs e)
+        {
+            array = new ArrayList(20);
+            for (int i = 0; i < array.Capacity; i++)
+            {
+                int y = i / array.Capacity * pnlSort.Height;
+                array.Add(y);
+            }
+            Randomize(array);
+            DrawSamples();
+        }
+
+        public void Randomize(IList list)
+        {
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                int swapIndex = randomNumber.Next(i + 1);
+                if (swapIndex != i)
+                {
+                    object tmp = list[swapIndex];
+                    list[swapIndex] = list[i];
+                    list[i] = tmp;
+                }
+            }
+        }
+
+        private void RedrawItem(int index, Graphics graphics, IList list)
+        {
+            int x = pnlSort.Width / array.Count * index;
+            graphics.DrawLine(new Pen(Color.White), new Point(x, 0), new Point(x, pnlSort.Height));
+            graphics.DrawLine(new Pen(Color.Black), new Point(x, pnlSort.Height), new Point(x, pnlSort.Height - (int)list[index]));
+        }
+    }
+}
